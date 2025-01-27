fe06c355976e48105fe17c719ef02753da741328
WajBlake <Family@DESKTOP-L32A5IQ>
2025-01-27 19:47:03 +0000

    Amend active state condition

--- PolicyReporter/PolicyReporter.Server/Services/PolicyReportService.cs
+++ PolicyReporter/PolicyReporter.Server/Services/PolicyReportService.cs
@@ -18,7 +18,7 @@ public class PolicyReportService(IPolicyRepository policyRepo) : IPolicyReportSe
     {
         DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
 
-        return GetPolicies(brokerNameOrId, filter: p => p.StartDate <= currentDate && p.EndDate >= currentDate);
+        return GetPolicies(brokerNameOrId, filter: p => p.StartDate <= currentDate && p.EndDate > currentDate);
     }
 
     private async Task<Report> GetPolicies(string? brokerNameOrId, Func<Policy, bool>? filter)
